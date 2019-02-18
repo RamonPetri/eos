f5e9595731ba22a2229064728b339529617473cc
ramon-vm <ramon.petri@student.hu.nl>
2019-02-18 15:43:03 +0100

    changed opdracht0

--- opdracht0/chiper.cpp
+++ opdracht0/chiper.cpp
@@ -18,16 +18,11 @@ std::string encrypt(std::string encrypt_string, int rotate){
 }
 
 
-int main(){
+int main(int argc, char * argv[]){
     std::string To_encrypt_string = "";
-    int num_of_rot = 0;
-    std::cout<<"Geef een woord om te versleutelen:  "<<std::endl;
-    std::cout<<"Woord of zin: ";
-    std::getline (std::cin,To_encrypt_string);
-    std::cout<<"Geef het aantal rotaties: "<<std::endl;
-    std::cout<<"Rotaties: ";
-    std::cin>>num_of_rot;
-    std::string encrypt_string = encrypt(To_encrypt_string, num_of_rot);
+    int rotate = (*argv[2] - '0'); 
+    std::cout<<rotate<<std::endl;
+    std::string encrypt_string = encrypt(argv[1], rotate);
     std::cout<<"Het versleutelde woord is: "<<encrypt_string<<std::endl;
 
 
